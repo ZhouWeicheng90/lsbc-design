f43f83adf8099a3d9351402875fa5f5197b6c968
Enne5w4 <enne5w4@126.com>
2020-01-06 20:35:09 +0800

    实现开发服务器

---
+++ build/server.js
@@ -0,0 +1,39 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var path = require('path')
+
+var server = http.createServer(function (request, response) {
+
+    //获取输入的url解析后的对象
+    var pathObj = url.parse(request.url == '/' ? '/demo.html' : request.url, false);
+    // request.url表示的是输入的url除了域名后面的部分，http://localhost:8080/login.html?aa=001&bb=002即为/login.html?aa=001&bb=002
+    // url.parse(request.url, true)表示的是将该url解析成一个对象。其中第二个参数默认false。
+    // false的话，query对应的值 =>    'aa=001&bb=002'
+    // true的话，query对应的值 =>{ aa: '001', bb: '002' }
+    // 相当于将url中的参数进行了解析，以便后面的调用(pathObj.query.aa)
+
+
+
+    //文件夹的绝对路径
+    var staticPath = path.resolve(__dirname, '../dist-docs')
+
+
+    //获取资源文件绝对路径
+    var filePath = path.join(staticPath, pathObj.pathname);
+    // path.join()用于连接路径。该方法的主要用途在于，会正确使用当前系统的路径分隔符，Unix系统是/，Windows系统是\。
+
+    //异步读取file
+    fs.readFile(filePath, 'binary', function (err, fileContent) {
+        if (err) {
+            response.writeHead(404, 'not found')
+            response.end('<h1>404 Not Found</h1>')
+        } else {
+            response.write(fileContent, 'binary')
+            response.end()
+        }
+    })
+
+})
+server.listen(1127)
+console.log('start success! please visit: http://localhost:1127/')
